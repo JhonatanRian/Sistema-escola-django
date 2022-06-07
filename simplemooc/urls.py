4a46c5d962f77c2762f56fcae356f128b5e30b32
Jhonatan Rian <jhonatanrian.jr@gmail.com>
2022-06-07 19:38:06 -0300

    crud admin

--- simplemooc/urls.py
+++ simplemooc/urls.py
@@ -4,6 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', 'simplemooc.core.views.home', name='home'),
+    url(r'^', include('simplemooc.core.urls', namespace='core')),
     url(r'^admin/', include(admin.site.urls)),
 )
